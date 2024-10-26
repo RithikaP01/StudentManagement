868f58a22a04e9f11cb620ddd4b90db04300332d
RithikaP01 <rithikapujary@gmail.com>
2024-10-26 15:29:48 +0400

    Initial commit

---
+++ StudentManagement/StudentManagement/Data/SchoolContext.cs
@@ -0,0 +1,23 @@
+﻿// Data/SchoolContext.cs
+using Microsoft.EntityFrameworkCore;
+using StudentManagement.Models;
+
+public class SchoolContext : DbContext
+{
+    public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
+
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Subject> Subjects { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>()
+            .HasOne(s => s.Subject)
+            .WithMany()
+            .HasForeignKey(s => s.SubjectId);
+
+        modelBuilder.Entity<Student>()
+            .Property(s => s.Remarks)
+            .HasComputedColumnSql("CASE WHEN Grade >= 75 THEN 'PASS' ELSE 'FAIL' END");
+    }
+}
---
+++ StudentManagement/StudentManagement/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentManagement.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int SubjectId { get; set; }  // Foreign key to Subject
+
+        [Required]
+        public int Grade { get; set; }
+
+        [NotMapped]  // Mark this as computed in the database, so we handle it ourselves
+        public string Remarks { get; set; }
+
+        // Navigation property to represent the relationship between Student and Subject
+        public virtual Subject Subject { get; set; }
+    }
+}
